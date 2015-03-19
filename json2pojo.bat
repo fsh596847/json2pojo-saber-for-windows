7f6816fd31a016c486b8ccd1005028df299f5420
JohnWatsonDev <johnwatson5412@gmail.com>
2015-03-19 14:19:24 +0800

    Update json2pojo.bat

--- json2pojo.bat
+++ json2pojo.bat
@@ -1,14 +1,4 @@
 @echo off
-:: rem 获取拖拽文件的名字(不含后缀名)
-set jsonName=%~n1
-:: rem 获取拖拽文件的绝对路径
 set fileAbsPath=%1
-:: rem 生成目标文件的相对路径
 set javaDir=javaTarget\%jsonName%
-
-:: rem 获取拖拽的json文件全名
-:: set jsonFile=%jsonName%.json
-
-:: rem .........do jsonschema2pojo command..........
-:: jsonschema2pojo --source %jsonFile% --target %javaDir% -a GSON -T JSON
 jsonschema2pojo --source %fileAbsPath% --target %javaDir% -a GSON -T JSON
